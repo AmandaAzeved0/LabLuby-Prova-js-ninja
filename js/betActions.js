7730d4efd0d7f4008e17e935df66ce6baab891e7
AmandaAzevedo0 <amanda.educacional@gmail.com>
2021-06-23 22:04:30 -0300

    numbers table added

---
+++ js/betActions.js
@@ -0,0 +1,6 @@
+const actionsButtons = document.getElementById('betActions')
+
+actionsButtons.innerHTML=`
+    <br/><br/>alooo
+
+`
--- js/createBets.js
+++ js/createBets.js
@@ -3,23 +3,30 @@ const content = document.getElementById('betsPageContent')
 content.innerHTML = `
 
     <div class="row">
-        <div class="col-2">
+        <div class="col">
             <strong class="text-uppercase newBetFor-text" id="newBetFor-text">New Bet For </strong>
             <span class="betType" id="betType">Mega sena</span>
         </div>
+        
         <div>
         <strong id="filters-text">Choose a game</strong>
         </div>
+        <br/>
         <div class="col">
-            
             <button class="btn " onclick="lotofacil()" id="lotofacil-btn"> Lotofácil</span>
             <button class="btn" onclick="megaSena()" id="megaSena-btn">Mega-Sena</span>
-            <button class="btn" onclick="lotomania()" id="lotomania-btn"> Lotomania</span>
+            <button class="btn" onclick="lotomania()" id="lotomania-btn"> Lotomania</button>
+            <br/><br/>
+            <strong id="filters-text">Fill your bet</strong>
+            <br/>
+            <strong id="gameType-description">descrição</strong>
+            <div id="numbers"></div>
+            <div id="betActions"></div>
         </div>
-        <div class="col-4" id="cart">
-        </div>
+        <div class="col-4" id="cart"></div>
+        
     </div>
-    
+
 
 `
 
